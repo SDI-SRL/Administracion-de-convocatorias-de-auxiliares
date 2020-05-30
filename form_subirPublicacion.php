8d8d25b86e952e21e7d71d17f77c011ca07ce644
SDI-SRL <62357101+SDI-SRL@users.noreply.github.com>
2020-05-30 02:15:12 -0400

    Update form_subirPublicacion.php

--- form_subirPublicacion.php
+++ form_subirPublicacion.php
@@ -10,7 +10,7 @@
     // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
     $s3 = new Aws\S3\S3Client([
         'version'  => '2006-03-01',
-        'region'   => 'us-east-2'',
+        'region'   => 'us-east-2',
     ]);
     $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
