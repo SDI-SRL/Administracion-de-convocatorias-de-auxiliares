0b7a1038252c0679a5ebfc21d6539bb12279a501
Freddy Jhonatan Claros Perez <freddyjhonatanclaros@gmail.com>
2020-06-21 22:28:54 -0400

    Modificacion en el proyecto y uso de PDO
    se cambio y organizo el proyecto para que este mas ordenado, asi como la implementacion de pdo

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -3,7 +3,7 @@
         protected $connexion_bd;
         public function Connexion(){
             try{
-                $this->connexion_bd = new PDO("pgsql:host=ec2-52-201-55-4.compute-1.amazonaws.com;dbname=ddm5k6l3g5nntm","erpgwqxdcmmizk","d764438378b6a33d99872ff2f4321949530f5f26e8271e10fb80ece8311e701a");
+                $this->connexion_bd = new PDO("pgsql:host=ec2-52-201-55-4.compute-1.amazonaws.com;port=5432;dbname=ddm5k6l3g5nntm","erpgwqxdcmmizk","d764438378b6a33d99872ff2f4321949530f5f26e8271e10fb80ece8311e701a");
                 $this->connexion_bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $this->connexion_bd;
             }catch(Exception $e){
@@ -11,6 +11,7 @@
                 echo "Error en la linea ".$e->getLine();
             }
         }
+        
 
     }
 
